574af83dd9848224cbceffdf4986e2486d161969
dinistavares <dinis@crisp.chat>
2024-05-06 10:05:15 +0100

    add relationships

--- relationships.go
+++ relationships.go
@@ -1,13 +1,20 @@
 package klaviyo
 
 type Relationships struct {
-	Lists        *RelationshipLists        `json:"lists,omitempty"`
-	Segments     *RelationshipSegments     `json:"segments,omitempty"`
-	Profile      *RelationshipProfile      `json:"profile,omitempty"`
-	Profiles     *RelationshipProfiles     `json:"profiles,omitempty"`
-	Tags         *RelationshipTags         `json:"tags,omitempty"`
-	Metric       *RelarionshipMetric       `json:"metric,omitempty"`
-	Attributions *RelationShipAttributions `json:"attributions,omitempty"`
+	Attributions         *RelationShipAttributions         `json:"attributions,omitempty"`
+	AttributedEvent      *RelationShipAttributedEvent      `json:"attributed-event,omitempty"`
+	Campaign             *RelationShipCampaign             `json:"campaign,omitempty"`
+	CampaignMessage      *RelationShipCampaignMessage      `json:"campaign-message,omitempty"`
+	Event                *RelationShipEvent                `json:"event,omitempty"`
+	Flow                 *RelationShipFlow                 `json:"flow,omitempty"`
+	FlowMessage          *RelationShipFlowMessage          `json:"flow-message,omitempty"`
+	FlowMessageVariation *RelationShipFlowMessageVariation `json:"low-message-variation,omitempty"`
+	Lists                *RelationshipLists                `json:"lists,omitempty"`
+	Metric               *RelarionshipMetric               `json:"metric,omitempty"`
+	Profile              *RelationshipProfile              `json:"profile,omitempty"`
+	Profiles             *RelationshipProfiles             `json:"profiles,omitempty"`
+	Segments             *RelationshipSegments             `json:"segments,omitempty"`
+	Tags                 *RelationshipTags                 `json:"tags,omitempty"`
 }
 
 type GenericRelationshipData struct {
@@ -15,11 +22,52 @@ type GenericRelationshipData struct {
 	ID   string `json:"id,omitempty"`
 }
 
+type RelationShipAttributions struct {
+	Data  *[]GenericRelationshipData `json:"data,omitempty"`
+	Links *GenericLinks              `json:"links,omitempty"`
+}
+
+type RelationShipAttributedEvent struct {
+	Data  *GenericRelationshipData `json:"data,omitempty"`
+	Links *GenericLinks            `json:"links,omitempty"`
+}
+
+type RelationShipCampaign struct {
+	Data  *GenericRelationshipData `json:"data,omitempty"`
+	Links *GenericLinks            `json:"links,omitempty"`
+}
+
+type RelationShipCampaignMessage struct {
+	Data  *GenericRelationshipData `json:"data,omitempty"`
+	Links *GenericLinks            `json:"links,omitempty"`
+}
+
+type RelationShipEvent struct {
+	Data  *Event        `json:"data,omitempty"`
+	Links *GenericLinks `json:"links,omitempty"`
+}
+
+type RelationShipFlow struct {
+	Data  *Event        `json:"data,omitempty"`
+	Links *GenericLinks `json:"links,omitempty"`
+}
+
+type RelationShipFlowMessage struct {
+	Data  *Event        `json:"data,omitempty"`
+	Links *GenericLinks `json:"links,omitempty"`
+}
+
+type RelationShipFlowMessageVariation struct {
+	Data  *Event        `json:"data,omitempty"`
+	Links *GenericLinks `json:"links,omitempty"`
+}
+
 type RelationshipLists struct {
 	Links *GenericLinks `json:"links,omitempty"`
 }
 
-type RelationshipSegments struct {
+type RelarionshipMetric struct {
+	Data  *Event        `json:"data,omitempty"`
 	Links *GenericLinks `json:"links,omitempty"`
 }
 
@@ -32,16 +80,10 @@ type RelationshipProfiles struct {
 	Links *GenericLinks `json:"links,omitempty"`
 }
 
+type RelationshipSegments struct {
+	Links *GenericLinks `json:"links,omitempty"`
+}
+
 type RelationshipTags struct {
 	Links *GenericLinks `json:"links,omitempty"`
-}
-
-type RelarionshipMetric struct {
-	Data  *Event        `json:"data,omitempty"`
-	Links *GenericLinks `json:"links,omitempty"`
-}
-
-type RelationShipAttributions struct {
-	Data  *[]GenericRelationshipData `json:"data,omitempty"`
-	Links *GenericLinks              `json:"links,omitempty"`
 }
