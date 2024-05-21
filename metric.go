7a43dfceff283629d4f83de4e5be8034258d520e
dinistavares <dinis@crisp.chat>
2024-05-21 09:46:35 +0100

    metric attributes

---
+++ metric.go
@@ -0,0 +1,30 @@
+package klaviyo
+
+import "time"
+
+type MetricAttributes struct {
+	Name        string             `json:"name,omitempty"`
+	Created     string             `json:"created,omitempty"`
+	Updated     *time.Time         `json:"updated,omitempty"`
+	Integration *MetricIntegration `json:"integration,omitempty"`
+}
+
+type Metric struct {
+	Type       string            `json:"type,omitempty"`
+	ID         string            `json:"id,omitempty"`
+	Attributes *MetricAttributes `json:"attributes,omitempty"`
+	Links      *GenericLinks     `json:"links,omitempty"`
+}
+
+type MetricIntegration struct {
+	Category *MetricCategory `json:"category,omitempty"`
+	ID       string          `json:"id,omitempty"`
+	Key      string          `json:"key,omitempty"`
+	Name     string          `json:"name,omitempty"`
+	Object   string          `json:"object,omitempty"`
+}
+
+type MetricCategory struct {
+	Category string `json:"category,omitempty"`
+	ID       int    `json:"id,omitempty"`
+}
